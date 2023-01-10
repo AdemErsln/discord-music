e2788c82199a4091ab98eb706d6ae2ff938d529e
Adem <100190744+warss7878@users.noreply.github.com>
2023-01-10 21:19:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-TOKEN: "",
+TOKEN: "MTA1MzA4NjYwMjk3MDI3MTc1NA.G2tXU4.fQfawN0Jqo-6pvcHXc8wATb0bvLMj3sCOASneM",
 ownerID: [""], //write your discord user id. example: ["id"] or ["id1","id2"]
 botInvite: "", //write your discord bot invite.
 supportServer: "", //write your discord bot support server invite.
